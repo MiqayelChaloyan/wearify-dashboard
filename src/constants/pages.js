503ad0f12815ac2f02043d05fbdfc04c61df3d17
MiqayelChaloyan <miqochaloyan@gmail.com>
2025-04-29 17:00:32 +0400

    -initial setup

---
+++ src/constants/pages.js
@@ -0,0 +1,9 @@
+const PAGES = {
+    SIGN_IN: '/sign-in',
+    RESET_PASSWORD: '/reset-password',
+    MAIN: '/'
+};
+
+export {
+    PAGES
+};
---
+++ src/pages/dashboard/main/index.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const Main = () => {
+    return (
+        <div>
+            Main
+        </div>
+    )
+};
+
+export default Main;
